eecb5d809d55cc655014a0f6291064b18bf7ec54
gilsane <20244444@sdhs.gwe.hs.kr>
2024-12-12 11:04:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@
 #파일 이름 streamlit_app.py
 
 import streamlit as st
-from fastai.vision.all import *
-from PIL import Image
-import gdown
+import pandas as pd
+import numpy as np
 
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
 
-
+st.dataframe(df)  # Same as st.write(df)
