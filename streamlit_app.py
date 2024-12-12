234c91069898ed5997829780397aa96ec1b06c0c
gilsane <20244444@sdhs.gwe.hs.kr>
2024-12-12 19:16:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import numpy as np  # numpy를 import해야 함
 st.title("Model Metadata Viewer")
 
 # GitHub Raw 파일 URL과 모델 유형
-GITHUB_RAW_URL = "https://github.com/gilsane/classtim/raw/refs/heads/main/xgb_model.pkl"
+GITHUB_RAW_URL = "https://github.com/gilsane/classtim/raw/refs/heads/main/xgb_model%20(1).pkl"
 MODEL_TYPE = "XGBoost"  # "fastai", "scikit-learn Random Forest", or "XGBoost"
 CSV_FILE_URL = "https://github.com/gilsane/classtim/raw/refs/heads/main/(%EC%8B%A4%EC%8A%B5%EC%9A%A9)%20%EC%95%84%ED%8C%8C%ED%8A%B8%EA%B0%80%20%EC%8B%A4%EA%B1%B0%EB%9E%98%20%EA%B0%80%EA%B3%B5%20%EB%8D%B0%EC%9D%B4%ED%84%B0%20(1).csv"
 
