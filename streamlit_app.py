67e1006d27b5fb7389b799a465d78e648f3cb3db
gilsane <20244444@sdhs.gwe.hs.kr>
2024-12-12 11:10:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,24 @@
-#이전 수업 시간에 만들었던 이미지 분류 pkl 파일을 바탕으로 한 이미지 분류 모델을 Streamlit에 올리는 예제 코드
-#파일 이름 streamlit_app.py
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+#csv 데이터 파일 경로(Github) 및 모델 파일 경로(Google Drive) 설정
 
-st.dataframe(df)  # Same as st.write(df)
+file_path = "https://github.com/gilsane/classtim/raw/refs/heads/main/(%EC%8B%A4%EC%8A%B5%EC%9A%A9)%20%EC%95%84%ED%8C%8C%ED%8A%B8%EA%B0%80%20%EC%8B%A4%EA%B1%B0%EB%9E%98%20%EA%B0%80%EA%B3%B5%20%EB%8D%B0%EC%9D%B4%ED%84%B0%20(1).csv"
+model_path = "1-9ILaJ0gX7a4aHNUUOWhY-vmp49-7KwX"
+
+# 파일 읽기
+try:
+    df = pd.read_csv(file_path, encoding='utf-8')
+except Exception:
+    try:
+        df = pd.read_csv(file_path, encoding='utf-8-sig')
+    except Exception:
+        try:
+            df = pd.read_csv(file_path, encoding='cp949')
+        except Exception:
+            with output:
+                st.write("모델을 읽어올 수 없습니다. 모델 파일 정보를 다시 한번 확인해주세요")
+            return
+
+st.dataframe(df.head()) 
